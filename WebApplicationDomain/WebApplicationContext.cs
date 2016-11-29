056e13b4c178f2efb206545f5bf158e720224b01
robin_8588 <robin_8588@aliyun.com>
2016-11-29 10:46:18 +0800

    add domain

---
+++ WebApplicationDomain/WebApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplicationDomain.Migrations;
+
+namespace WebApplicationDomain
+{
+    public class WebApplicationContext:DbContext
+    {
+        public WebApplicationContext() : base("WebApplicationConnection")
+        {
+
+        }
+
+        static WebApplicationContext()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<WebApplicationContext, Configuration>());
+        }
+    }
+}
